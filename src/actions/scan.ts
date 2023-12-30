06b1a4d7e7f19f3d9c21f1f26a18b0f8f4ce4a82
olivia <1096337364@qq.com>
2023-12-30 20:09:54 +1300

    remove qrcode generator

--- src/actions/scan.ts
+++ src/actions/scan.ts
@@ -1,11 +1,8 @@
-import qrcodeTerminal from "qrcode-terminal";
-
 export function onScan(qrcode) {
-  qrcodeTerminal.generate(qrcode, { small: true }); // 在console端显示二维码
   const qrcodeImageUrl = [
     "https://api.qrserver.com/v1/create-qr-code/?data=",
     encodeURIComponent(qrcode),
   ].join("");
 
-  console.log(qrcodeImageUrl);
+  console.log(`-------------${qrcodeImageUrl}-------------`);
 }
